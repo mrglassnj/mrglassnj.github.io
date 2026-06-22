bcb92d619557decac602a2f66af48f36a5767cf3
isaacherbst613 <74846840+isaacherbst613@users.noreply.github.com>
2026-06-22 13:32:02 -0400

    gdrive config

--- assets/js/config.js
+++ assets/js/config.js
@@ -10,7 +10,7 @@ window.MRGLASS_CONFIG = {
   /* 1) Your Google Drive API key.
         Leave it as "" to show sample placeholder tiles.
         Paste your key here once you've created it (see README). */
-  apiKey: "",
+  apiKey: "AIzaSyBLWKbV2ba7wdu3z1YVoyzagMTwnn7lqUY",
 
   /* 2) Your gallery categories.
         - "name"     = the label shown on the website + filter button
@@ -18,11 +18,11 @@ window.MRGLASS_CONFIG = {
                        (the long code in the folder's share URL).
         Leave folderId as "" and a category will show sample tiles. */
   categories: [
-    { name: "Shower Enclosures", folderId: "" },
-    { name: "Custom Mirrors",    folderId: "" },
-    { name: "Glass Railings",    folderId: "" },
-    { name: "Storefronts",       folderId: "" },
-    { name: "Office Partitions", folderId: "" }
+    { name: "Shower Enclosures", folderId: "https://drive.google.com/drive/folders/1VUu1-awi0LouqE8cisalEf7qDQBF9Tbk?usp=drive_link" },
+    { name: "Custom Mirrors",    folderId: "https://drive.google.com/drive/folders/1j1w9MaUwToR6X43yQ9gouXaCQahMDD5b?usp=drive_link" },
+    { name: "Glass Railings",    folderId: "https://drive.google.com/drive/folders/1yUefuUEx2PQi5A9PqAEw3ThwW7y439_-?usp=drive_link" },
+    { name: "Storefronts",       folderId: "https://drive.google.com/drive/folders/1Y_DL8RLUnZya3KKZ-WLQPeyrqnHweEsr?usp=drive_link" },
+    { name: "Office Partitions", folderId: "https://drive.google.com/drive/folders/1coUFzbk-Ot6Z84YdGxnYADG52KArLPWS?usp=drive_link" }
   ]
 
 };
